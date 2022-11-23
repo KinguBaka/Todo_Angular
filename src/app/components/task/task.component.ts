477c10004254d310a146c2ac795bc7a3675726d2
KinguBaka <tarikab92@yahoo.fr>
2022-11-23 16:48:15 +0100

    mise en place project + add task component

---
+++ src/app/components/task/task.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.scss']
+})
+export class TaskComponent {
+  public name : string;
+  public complete:boolean ;
+  public spanStyle: string;
+  public liStyle: string;
+
+  constructor() {
+    this.name = "Tarik";
+    this.complete = false;
+    this.spanStyle ='';
+    this.liStyle =''
+  }
+
+  toogleComplete() {
+    this.complete = this.complete  ? false : true
+  }
+
+  getButtonText() {
+    return this.complete ? "Terminer" : "Annuler"
+  }
+
+  getComplete():string | void {
+    if (this.complete) {
+      return this.name;
+    }
+  }
+
+  getBadgeVariant():string {
+    if (this.complete) {
+      this.spanStyle = 'd-inline float-right badge badge-success'
+    } else {
+      this.spanStyle = 'd-inline float-right badge badge-warning'
+    }
+    return this.spanStyle;
+  }
+
+  getItemVariant():string {
+    if (this.complete) {
+      this.liStyle = 'list-group-item list-group-item-success'
+    } else {
+      this.liStyle = 'list-group-item list-group-item-warning'
+    }
+    return this.liStyle;
+  }
+
+  classBtn():string {
+    return this.complete ? "btn btn-outline-success" : "btn btn-outline-warning"
+  }
+
+}
