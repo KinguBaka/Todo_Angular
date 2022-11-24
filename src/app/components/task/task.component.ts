cb7999b3b9f03bc072ff95dde95bd78751ddd737
KinguBaka <tarikab92@yahoo.fr>
2022-11-24 11:55:53 +0100

    add commentary

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -12,26 +12,32 @@ export class TaskComponent {
     this.complete = false;
   }
 
+  // Set the Status on true | false
   toogleComplete() {
     this.complete = !this.complete
   }
 
-  getButtonText() {
+  // Change value of Btn
+  getButtonText():string {
     return this.complete ? "Annuler" : "Fait"
   }
 
+  // Change color Btn
   classBtn():string {
     return this.complete ? "btn btn-danger" : "btn btn-success"
   }
 
+  // Change color badge
   setBadgeClass():string {
     return this.complete ? "bg-success" : "bg-danger"
   }
 
+  // Change value of badge
   setBadge():string {
     return this.complete ? "Fait" : "A faire"
   }
 
+  // Change class Card (scss)
   classCard():string {
     return this.complete ? "finish" : "notFinish"
   }
