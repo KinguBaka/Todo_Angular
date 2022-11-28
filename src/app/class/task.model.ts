1b94e260327e06425eed9067aa0fe2c8dab6bbed
KinguBaka <tarikab92@yahoo.fr>
2022-11-28 12:37:40 +0100

    ajout de la date !

--- src/app/class/task.model.ts
+++ src/app/class/task.model.ts
@@ -3,11 +3,13 @@ export class Task {
   title:string;
   completed:boolean;
   description:string
+  created:string;
 
-  constructor(id:number, title:string, completed:boolean, description:string ) {
+  constructor(id:number, title:string, completed:boolean, description:string) {
     this.id = id;
     this.title = title;
     this.completed = completed;
     this.description = description;
+    this.created = Date()
   }
 }
