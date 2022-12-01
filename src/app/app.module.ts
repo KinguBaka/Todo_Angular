a2929fa8a1c1ea16233bc798620dbc42fdda3d7f
KinguBaka <tarikab92@yahoo.fr>
2022-12-01 10:42:16 +0100

    création component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppComponent } from './app.component';
 import { TaskComponent } from './components/task/task.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { TodolistComponent } from './components/todolist/todolist.component';
+import { NavComponent } from './components/nav/nav.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent
+    TaskComponent,
+    TodolistComponent,
+    NavComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
