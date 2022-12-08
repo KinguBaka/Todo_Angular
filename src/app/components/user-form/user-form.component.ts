bbc290f02e3632fdf51aa403b071b37e27b17359
KinguBaka <tarikab92@yahoo.fr>
2022-12-08 16:47:18 +0100

    user-form skills + css ok

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 
@@ -15,22 +15,36 @@ export class UserFormComponent {
   lastName: FormControl;
   email: FormControl;
   team: FormControl;
+  skills: FormArray;
+
   constructor(private fb: FormBuilder, public userService: UserService, public router: Router) {
 
     this.firstName = this.fb.control('', [Validators.required]);
     this.lastName = this.fb.control('', [Validators.required]);
     this.email = this.fb.control('', [Validators.required, Validators.email]);
     this.team = this.fb.control('', [Validators.required]);
+    this.skills = this.fb.array(['']);
 
     this.userForm = this.fb.group({
       firstName: this.firstName,
       lastName: this.lastName,
       email: this.email,
-      team: this.team
+      team: this.team,
+      skills : this.skills
+
     });
   }
 
+  addSkill() {
+    this.skills.push(this.fb.control(''))
+  }
+
+  removeSkill() {
+    this.skills.removeAt(- 1)
+  }
+
   onSubmit() {
+    this.userForm.get("skills") as FormArray
     this.userService.addUser(this.userForm.value);
     this.router.navigate(['user-list']);
   }
